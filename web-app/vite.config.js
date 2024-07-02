1031536230e0ec7694ca950f90cf613b14c20e8c
Felipe Brum <fbrump@gmail.com>
2024-07-02 13:09:31 +0100

    feat: Create APP env
    - Add development
    - Add staging
    - Add production
    - Change compose
    
    ticket: #45

--- web-app/vite.config.js
+++ web-app/vite.config.js
@@ -1,32 +1,38 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const serverConfig = {
     proxy: {
       '/api-core': {
-        target: 'http://localhost:8000/api/v1/',
+        target: env.VITE_API_CORE_HOST + '/api/v1/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api-core/, ''),
       },
       '/api-auth': {
-        target: 'http://localhost:8000/api/v1/',
+        target: env.VITE_API_CORE_HOST + '/api/v1/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api-auth/, ''),
       }
     }
+  }
+
+  return {
+    plugins: [
+      vue(),
+      VueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: serverConfig
   }
 })
